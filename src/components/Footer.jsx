57494f6754e474f45896064b942bbc4cb33abdb4
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2024-04-18 14:51:33 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="text-center py-4">
+      <p class="text-gray-600">
+        &copy;{" "}
+        <a href="https://www.youtube.com/@code-with-Bharadwaj" class="text-gray-600 hover:underline">
+         Youtube - @code-with-Bharadwaj
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
